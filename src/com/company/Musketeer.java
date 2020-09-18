4c78d60e219f6e56841211a2ee8e1c577fd3a59a
Angela Crepaldi <angelacrepaldi@gmail.com>
2020-09-18 10:07:51 -0300

    Musketeer.java: Added member variables age and name, getter and setter functions for name and age, constructos and default age.

--- src/com/company/Musketeer.java
+++ src/com/company/Musketeer.java
@@ -5,6 +5,35 @@ public class Musketeer {
     private String m_name;
     private int m_age;
 
-    // Constructors
-    
+    // Constructor
+    public Musketeer(String name, int age){
+        this.setName(name);
+        this.m_age = age;
+    }
+
+    // Default age for all Musketeers
+    public static final int S_DEFAULT_AGE = 35;
+
+    // Second constructor
+    public Musketeer(String name) {
+        this.setName(name);
+        this.m_age = S_DEFAULT_AGE;
+    }
+
+    // Getter and setter functions
+    public void setAge(int age) {
+        this.m_age = age;
+    }
+
+    public int getAge() {
+        return this.m_age;
+    }
+
+    public String getName() {
+        return m_name;
+    }
+
+    public void setName(String m_name) {
+        this.m_name = m_name;
+    }
 }
